ee13e2a50a7972bf4196c268bdafdcfc67b8150d
Hyma <hymatamarana2000@gmail.com>
2024-04-14 18:52:58 +0530

    adding technology

---
+++ index.js
@@ -0,0 +1,13 @@
+import './index.css'
+const CardItem=(props)=>{
+    const{cardDetails}=props;
+    const{title,description,imgUrl,className}=cardDetails;
+    <li className={`${className} card-item`}>
+        <div>
+            <h1 className="card-title">{title}</h1>
+            <p className="card-description">{description}</p>
+            <img src={imgUrl} alt={title} className="card-img"/>
+        </div>
+    </li>
+};
+export default CardItem;
